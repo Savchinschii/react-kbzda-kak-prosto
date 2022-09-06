6ad9b480e965af2a783993d1da51d12029398fce
Savcinschi Egor <sasha_lagun1995@mail.ru>
2022-09-06 19:21:32 +0300

    conditional rendering and ternary expressions and hook use state

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,21 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./Components/Accordion/Accordion";
 import {Rating} from "./Components/Rating/Rating";
+import {OnOff} from "./Components/OnOff/OnOff";
 
 function App() {
     console.log('App rendering')
     return (
         <div>
-            <PageTitle title={'This is APP component'}/>
-            <PageTitle title={'My friends'}/>
-            <Rating value={3}/>
-            <Accordion titleValue={'Menu'} />
-            <Accordion titleValue={'Users'}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <OnOff/>
+            {/*<Accordion titleValue={'Menu'}  collapsed={true} />*/}
+            {/*<Accordion titleValue={'Users'} collapsed={false}/>*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
         </div>
     );
 }
--- src/Components/Accordion/Accordion.tsx
+++ src/Components/Accordion/Accordion.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 type AccordionPropsType = {
     titleValue: string
+    collapsed:boolean
 }
 
 
 export function Accordion(props: AccordionPropsType) {
     console.log('Accordion rendering')
-    return (
-        <div>
-            <AccordionTitle title={props.titleValue}/>
-            <AccordionBody/>
-        </div>
-    )
+    return <div>
+        <AccordionTitle title={props.titleValue}/>
+        { !props.collapsed && <AccordionBody/>}
+    </div>
+
 }
 
 type AccordionTitlePropsType = {
     title: string
 }
 
-function AccordionTitle(props:AccordionTitlePropsType) {
+export function AccordionTitle(props:AccordionTitlePropsType) {
     return <h3>{props.title}</h3>
+
 }
 
 function AccordionBody() {
