9659cc9d4c2bbaef4096d1079070922365b5387e
Savcinschi Egor <sasha_lagun1995@mail.ru>
2022-09-12 20:20:16 +0300

    storybook:component stories

--- src/Components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/Components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -2,7 +2,6 @@ import {useState} from "react";
 
 type AccordionPropsType = {
     titleValue: string
-    // collapsed:boolean
 }
 
 
--- src/Components/UncontrolledOnOff/UncontrolledOnOff.tsx
+++ src/Components/UncontrolledOnOff/UncontrolledOnOff.tsx
@@ -2,12 +2,14 @@ import {useState} from "react";
 
 export type PropsType = {
     onChange: (on: boolean)=> void
+    defaultOn?: boolean
+
 
 }
 
 export  function UncontrolledOnOff(props: PropsType) {
 
-    let [on, setOn] = useState(false)
+    let [on, setOn] = useState(props.defaultOn ? props.defaultOn : false)
 
     const onStyle = {
         width: '30px',
